9a1356e81c20fa852e8a0ff754179fbbb7b5c110
juguarnieri <juliaguarnieri04@gmail.com>
2025-03-13 19:38:02 -0300

    feat: creating the correct structure with use effect

---
+++ componentes/Button.jsx
@@ -0,0 +1,10 @@
+// componentes/Button.jsx
+import styles from "../styles/Button.module.css";
+
+export default function Button({ text, onClick, variant = "primary" }) {
+    return (
+        <button className={`${styles.button} ${styles[variant]}`} onClick={onClick}>
+            {text}
+        </button>
+    );
+}
